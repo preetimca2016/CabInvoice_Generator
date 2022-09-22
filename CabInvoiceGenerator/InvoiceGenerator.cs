dae26afff15e79cc541e5dde87b054912aa575b4
“preetimca2016” <preetimca2016@gmail.com>
2022-09-23 00:37:39 +0530

    [Preeti] Add Test Case For Total fare

---
+++ CabInvoiceGenerator/InvoiceGenerator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CabInvoiceGenerator
+{
+    public class InvoiceGenerator
+    {
+        public readonly int MINIMUM_FARE;
+        public readonly int COST_PER_KM;
+        public readonly int COST_PER_MINUTE;
+        public RideType rideType;
+
+        public InvoiceGenerator(RideType rideType)
+        {
+            this.rideType = rideType;
+            if (rideType == RideType.NORMAL)
+            {
+                MINIMUM_FARE = 5;
+                COST_PER_KM = 10;
+                COST_PER_MINUTE = 1;
+            }
+            else
+            {
+                MINIMUM_FARE = 20;
+                COST_PER_KM = 15;
+                COST_PER_MINUTE = 2;
+            }
+        }
+        // total fare calculate
+        public double CalculateFare(double distance, int time)
+        {
+            double totalFare = 0;
+            if (distance < 0)
+                throw new CabInvoiceCustomException(CabInvoiceCustomException.ExceptionType.INVALID_DISTANCE, "Distance cannot be zero");
+            else if (time < 0)
+                throw new CabInvoiceCustomException(CabInvoiceCustomException.ExceptionType.INVALID_TIME, "Time cannot be zero");
+            else
+            {
+                totalFare = distance * COST_PER_KM + time * COST_PER_MINUTE;
+            }
+            return Math.Max(totalFare, MINIMUM_FARE);
+        }
+    }
+}
---
+++ CabInvoiceGenerator/InvoiceSummary.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CabInvoiceGenerator
+{
+    public class InvoiceSummary
+    {
+        private int numberOfRides;
+        private double totalFare;
+        private double averageFare;
+
+        public InvoiceSummary(int numberOfRides, double totalFare)
+        {
+            this.numberOfRides = numberOfRides;
+            this.totalFare = totalFare;
+            this.averageFare = totalFare / numberOfRides;
+        }
+        public virtual bool Equals(Object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+            if (!(obj is InvoiceSummary))
+            {
+                return false;
+            }
+            InvoiceSummary inputedObject = (InvoiceSummary)obj;
+            return this.numberOfRides == inputedObject.numberOfRides && this.totalFare == inputedObject.totalFare && this.averageFare == inputedObject.averageFare; 
+        }
+    }
+}
